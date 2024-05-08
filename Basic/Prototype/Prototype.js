2c484e7e1ad51e2be2b09f91e3c97bbf6f592a30
shivam501-00 <khedekarshivam333@gmail.com>
2024-05-09 00:42:14 +0530

    Prototype

---
+++ Basic/Prototype/Prototype.js
@@ -0,0 +1,10 @@
+let myHeros= ["thor", "spiderman"]
+
+let heroPower ={
+    thor: "hammer",
+    spiderman: " sling",
+
+    getSpiderPower: function(){
+        console.log(`spidy power is ${this.spiderman}`)
+    }
+}
---
+++ Basic/Prototype/Prototype.md
@@ -0,0 +1,4 @@
+#Prototype
+
+- In JS every object hav internal property which is protorype. It might be refering to null value or another object.
+
